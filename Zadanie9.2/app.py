7b09b69d9922e0fe7872d6203d73d19830d11785
Maciej Ledzion <maciejledzion96@gmail.com>
2021-06-10 22:50:43 +0200

    update

--- Zadanie9.2/app.py
+++ Zadanie9.2/app.py
@@ -3,27 +3,25 @@ from flask import Flask, render_template, request
 import csv
 
 app = Flask(__name__)
+response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+data = response.json()
+rates = []
+for item in data:
+    for data_item in item['rates']:
+        rates.append(data_item)
+keys = rates[0].keys()
 
+a_file = open("my_csv.csv", "w")
+dict_writer = csv.DictWriter(a_file, keys, delimiter=";")
+dict_writer.writeheader()
+dict_writer.writerows(rates)
+a_file.close()
 
 @app.route('/formularz', methods=['GET' , 'POST'])
 def formularz():
-    if request.method == 'GET':
-        return render_template("formularz.html")
-    elif request.method == 'POST':
-        response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-        data = response.json()
-        rates = []
-        for item in data:
-            for data_item in item['rates']:
-                rates.append(data_item)
+    return render_template("formularz.html")
 
-        keys = rates[0].keys()
 
-        a_file = open("my_csv.csv", "w")
-        dict_writer = csv.DictWriter(a_file, keys, delimiter=";")
-        dict_writer.writeheader()
-        dict_writer.writerows(rates)
-        a_file.close()
 
 
 
